ecf5488cf73bab209a655d6e381aa0e2d63b031e
Sriharsha Aitharaju <68801258+HarshBrownsVI@users.noreply.github.com>
2021-07-16 23:30:19 -0400

    Update Task.java

--- src/main/java/ucf/assignments/Task.java
+++ src/main/java/ucf/assignments/Task.java
@@ -4,12 +4,18 @@
  */
 package ucf.assignments;
 
+import javafx.beans.property.SimpleStringProperty;
+
+import java.nio.charset.StandardCharsets;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 
 public class Task
 {
     // create a String to store the description
-    String description;
+    SimpleStringProperty description;
     // create a date picker variable to the due date
     LocalDate dueDate;
     // create a boolean variable to store whether the task is completed or not
@@ -19,44 +25,51 @@ public class Task
     public Task(String description, LocalDate dueDate, boolean isCompleted)
     {
         // set the description parameter equal to the description field
-        this.description = description;
+        this.description = new SimpleStringProperty(description);
         // set the dueDare parameter equal to the dueDate field
        this.dueDate = dueDate;
         // set the isCompleted parameter to be equal the field is Completed
         this.isCompleted = isCompleted;
     }
-    public void setDescription(String description)
+    public void setDescription(String des)
     {
         // set this.description equal to the field in the class
-        this.description = description;
+        description.set(des);
     }
     public void setDueDate(LocalDate dueDate){
         // set the due date equal to the due date field in the class
         this.dueDate = dueDate;
     }
-    /*public StringProperty descriptionProperty() {
-        if (description == null) description = new SimpleStringProperty(this, "firstName");
-        return description;
-    }*/
+
     public String getDescription(){
         // return the description field
-        return description;
+        return description.get();
     }
     public LocalDate getDueDate(){
         // return the date field
             return dueDate;
     }
+    
+    // return yes if the task is completed, otherwise no
     public String getIsCompleted()
     {
         if(isCompleted)
             return "yes";
         return "no";
     }
-    public void setIsCompleted(boolean completed)
-    {
-        // set the completed parameter equal to the isCompleted field
-        this.isCompleted = isCompleted;
+   
+    // check to see whether the date is valid or not
+    public boolean isValid(String dateStr) {
+        DateFormat sdf = new SimpleDateFormat(dueDate.toString());
+        sdf.setLenient(false);
+        try {
+            sdf.parse(dateStr);
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
     }
+    // return the item 
     public String toString(){
         return description + "," + dueDate + "," + isCompleted;
     }
